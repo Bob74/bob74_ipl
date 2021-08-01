2a0a534ffc303c5f58f0f46857ccf0d836105cf2
Michael S <mikispector2@gmail.com>
2021-08-01 19:02:25 +0300

    Fix client error if game build not sufficient

--- client.lua
+++ client.lua
@@ -140,7 +140,9 @@ Citizen.CreateThread(function()
     -- ====================================================================
     -- =------------------- [DLC: Diamond Casino Resort] -----------------=
     -- ====================================================================
-    DiamondCasino.LoadDefault()
-    DiamondPenthouse.LoadDefault()
+    if GetGameBuildNumber() >= 2060 then
+        DiamondCasino.LoadDefault()                -- 1100.000, 220.000, -50.000
+        DiamondPenthouse.LoadDefault()             -- 976.636, 70.295, 115.164
+    end
 end)
 
