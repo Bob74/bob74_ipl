0716047a1fa1cf7f4fb9579fc4d443003c7402fc
Sqrl <pigbird684@gmail.com>
2024-08-13 23:21:28 -0700

    feat: add different versions of pillbox

--- gtav/pillbox_hospital.lua
+++ gtav/pillbox_hospital.lua
@@ -4,9 +4,31 @@ exports('GetPillboxHospitalObject', function()
 end)
 
 PillboxHospital = {
-    ipl = "rc12b_default",
+    iplFixed = 'RC12B_Fixed',
+    iplDefault = 'RC12B_Default',
+    iplDestroyed = 'RC12B_Destroyed',
+    iplHospitalInterior = 'RC12B_HospitalInterior',
 
-    Enable = function(state)
-        EnableIpl(PillboxHospital.ipl, state)
+    LoadDefault = function(state)
+        PillboxHospital.DisableIpl()
+        EnableIpl(PillboxHospital.iplDefault, state)
+    end,
+
+    LoadFixed = function(state)
+        PillboxHospital.DisableIpl()
+        EnableIpl(PillboxHospital.iplFixed, state)
+    end,
+
+    LoadDestroyed = function(state)
+        PillboxHospital.DisableIpl()
+        EnableIpl(PillboxHospital.iplDestroyed, state)
+        EnableIpl(PillboxHospital.iplHospitalInterior, state)
+    end,
+
+    DisableIpl = function()
+        RemoveIpl("RC12B_Fixed")
+        RemoveIpl("RC12B_Default")
+        RemoveIpl('RC12B_Destroyed')
+        RemoveIpl("RC12B_HospitalInterior")
     end
 }
