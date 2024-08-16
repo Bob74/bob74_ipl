e934d55dfebc8a2533f42758b1e536d58672282c
Sqrl <pigbird684@gmail.com>
2024-08-16 02:17:39 -0700

    feat: add nightclub basement and entityset

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -101,6 +101,7 @@ client_scripts {
 
     -- DLC After Hours
     , "dlc_afterhours/nightclubs.lua"
+    , "dlc_afterhours/nightclubbasement.lua"
 
     -- Dlc Arena War
     , "dlc_arenawar/interior.lua"
