cc8ff85f5b14a125e5e4f85eef5f604ae785f194
MaaximGL <hyperconceptx@gmail.com>
2024-08-21 01:03:50 +0300

    [mp2024_01]: added carrier EnitySet in 3 ineriors

--- dlc_summer/carrier.lua
+++ dlc_summer/carrier.lua
@@ -4,6 +4,7 @@ exports('GetSummerCarrierObject', function()
 end)
 
 SummerCarrier = {
+
     ipl = {
         "m24_1_carrier",
         "m24_1_carrier_int1",
@@ -19,7 +20,110 @@ SummerCarrier = {
         EnableIpl(SummerCarrier.ipl, state)
     end,
 
+    Interior1 = {
+        interiorId = 295681,
+
+        Storage = {
+            open = "set_hangar_1_spwn_cbd_open",
+            closed = "set_hangar_1_spwn_cbd_closed",
+    
+            Set = function(storage, refresh)
+                SummerCarrier.Interior1.Storage.Clear(false)
+                SetIplPropState(SummerCarrier.Interior1.interiorId, storage, true, refresh)
+            end,
+            Clear = function(refresh)
+                SetIplPropState(SummerCarrier.Interior1.interiorId, {
+                    SummerCarrier.Interior1.Storage.open,
+                    SummerCarrier.Interior1.Storage.closed
+                }, false, refresh)
+            end
+        },
+    },
+
+    Interior2 = {
+        interiorId = 296193,
+
+        Door = {
+            open = "set_hangar_2_doors_open",
+            closed = "set_hangar_2_doors_closed",
+    
+            Set = function(door, refresh)
+                SummerCarrier.Interior2.Door.Clear(false)
+                SetIplPropState(SummerCarrier.Interior2.interiorId, door, true, refresh)
+            end,
+            Clear = function(refresh)
+                SetIplPropState(SummerCarrier.Interior2.interiorId, {
+                    SummerCarrier.Interior2.Door.open,
+                    SummerCarrier.Interior2.Door.closed
+                }, false, refresh)
+            end
+        },
+
+        Storage = {
+            open = "set_hangar_2_spwn_cbd_open",
+            closed = "set_hangar_2_spwn_cbd_closed",
+    
+            Set = function(storage, refresh)
+                SummerCarrier.Interior2.Storage.Clear(false)
+                SetIplPropState(SummerCarrier.Interior2.interiorId, storage, true, refresh)
+            end,
+            Clear = function(refresh)
+                SetIplPropState(SummerCarrier.Interior2.interiorId, {
+                    SummerCarrier.Interior2.Storage.open,
+                    SummerCarrier.Interior2.Storage.closed
+                }, false, refresh)
+            end
+        },
+    },
+
+    Interior3 = {
+        interiorId = 296449,
+
+        Door = {
+            open = "set_hangar_3_doors_open",
+            closed = "set_hangar_3_doors_closed",
+    
+            Set = function(door, refresh)
+                SummerCarrier.Interior3.Door.Clear(false)
+                SetIplPropState(SummerCarrier.Interior3.interiorId, door, true, refresh)
+            end,
+            Clear = function(refresh)
+                SetIplPropState(SummerCarrier.Interior3.interiorId, {
+                    SummerCarrier.Interior3.Door.open,
+                    SummerCarrier.Interior3.Door.closed
+                }, false, refresh)
+            end
+        },
+
+        Storage = {
+            open = "set_hangar_3_spwn_cbd_open",
+            closed = "set_hangar_3_spwn_cbd_closed",
+    
+            Set = function(storage, refresh)
+                SummerCarrier.Interior3.Storage.Clear(false)
+                SetIplPropState(SummerCarrier.Interior3.interiorId, storage, true, refresh)
+            end,
+            Clear = function(refresh)
+                SetIplPropState(SummerCarrier.Interior3.interiorId, {
+                    SummerCarrier.Interior3.Storage.open,
+                    SummerCarrier.Interior3.Storage.closed
+                }, false, refresh)
+            end
+        },
+    },
+
     LoadDefault = function()
         SummerCarrier.Enable(true)
+
+        SummerCarrier.Interior2.Door.Set(SummerCarrier.Interior2.Door.closed, false)
+        SummerCarrier.Interior3.Door.Set(SummerCarrier.Interior3.Door.open, false)
+
+        SummerCarrier.Interior1.Storage.Set(SummerCarrier.Interior1.Storage.closed, false)
+        SummerCarrier.Interior2.Storage.Set(SummerCarrier.Interior2.Storage.closed, false)
+        SummerCarrier.Interior3.Storage.Set(SummerCarrier.Interior3.Storage.closed, false)
+
+        RefreshInterior(SummerCarrier.Interior1.interiorId)
+        RefreshInterior(SummerCarrier.Interior2.interiorId)
+        RefreshInterior(SummerCarrier.Interior3.interiorId)
     end
 }
